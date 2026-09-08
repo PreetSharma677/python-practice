23d47ec6c9fdee142266ac057e1cf006f593f9f6
PreetSharma677 <cherychery620@gmail.com>
2026-09-08 22:14:49 +0530

    Add files via upload

---
+++ guess_game_v2.py
@@ -0,0 +1,16 @@
+import random
+
+number = random.randint(1, 10)
+attempts = 0
+
+while True:
+    guess = int(input("Guess the number: "))
+    attempts += 1
+
+    if guess == number:
+        print(f"Correct! You guessed it in {attempts} tries.")
+        break
+    elif guess < number:
+        print("Too low!")
+    else:
+        print("Too high!")
